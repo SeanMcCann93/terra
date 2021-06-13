1ae082f7a91b7d1621fb0c7b8fb211588a47d0eb
SeanMc93 <SeanMc93>
2021-06-14 00:41:26 +0100

    'Could still need work, works for one use.' ~ 14/06/21 @ 00:41:26

--- addTERRA.sh
+++ addTERRA.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "ALL​​​​​​​​​​ ALL=(root) NOPASSWD:/usr/bin/update-alternatives --config terraform" >> sudo /etc/sudoers.d/terraform
+curUser=$(whoami)
+if [[ -z $(sudo ls /etc/sudoers.d/ | grep "terraform") ]] || [[ -z $(sudo cat /etc/sudoers.d/terraform | grep "​​​​​​${currentUser} ALL=(root) NOPASSWD:/usr/bin/update-alternatives --config terraform") ]]; then
+    sudo bash -c 'echo "${currentUser} ALL=(root) NOPASSWD:/usr/bin/update-alternatives --config terraform" >> /etc/sudoers.d/terraform'
+fi
 
 chmod 755 ./*.sh
 
