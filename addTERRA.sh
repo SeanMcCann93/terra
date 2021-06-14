186f8c2f4a8d4b8368ecb7990a7f3e7c77626f76
SeanMcCann93 <78518393+SeanMcCann93@users.noreply.github.com>
2021-06-14 02:09:22 +0100

    Fix delete current (#2)
    * 'Could still need work, works for one use.' ~ 14/06/21 @ 00:41:26
    
    * 'Deactivate destructive code' ~ 14/06/21 @ 01:36:09
    
    * 'Remove sudoers command - sudo access needed!' ~ 14/06/21 @ 02:03:31
    
    Co-authored-by: SeanMc93 <SeanMc93>

--- addTERRA.sh
+++ addTERRA.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "ALL​​​​​​​​​​ ALL=(root) NOPASSWD:/usr/bin/update-alternatives --config terraform" >> sudo /etc/sudoers.d/terraform
-
 chmod 755 ./*.sh
 
 sudo cp ./TERRA.sh /bin/terra || exit 1
