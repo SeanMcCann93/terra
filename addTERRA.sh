31620865e4ec79ff4c43f291f49909c47ff44c12
SeanMc93 <SeanMc93>
2021-06-14 01:36:09 +0100

    'Deactivate destructive code' ~ 14/06/21 @ 01:36:09

--- addTERRA.sh
+++ addTERRA.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-curUser=$(whoami)
-if [[ -z $(sudo ls /etc/sudoers.d/ | grep "terraform") ]] || [[ -z $(sudo cat /etc/sudoers.d/terraform | grep "​​​​​​${currentUser} ALL=(root) NOPASSWD:/usr/bin/update-alternatives --config terraform") ]]; then
-    sudo bash -c 'echo "${currentUser} ALL=(root) NOPASSWD:/usr/bin/update-alternatives --config terraform" >> /etc/sudoers.d/terraform'
-fi
+# curUser=$(whoami)
+# if [[ ${curUser} == "root" ]]; then
+#     echo "Can not assign root to file."
+# elif [[ -z $(sudo ls /etc/sudoers.d/ | grep "terraform") ]] || [[ -z $(sudo cat /etc/sudoers.d/terraform | grep "​​​​​​${curUser} ALL=(root) NOPASSWD:/usr/bin/update-alternatives --config terraform") ]]; then
+#     sudo bash -c 'echo "${curUser} ALL=(root) NOPASSWD:/usr/bin/update-alternatives --config terraform" >> /etc/sudoers.d/terraform'
+# fi
 
 chmod 755 ./*.sh
 
